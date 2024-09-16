2d1083bff42dc2f08fdbb576a6de19a303245959
Daniel Okuno <danielkenji13@yahoo.com.br>
2024-09-16 16:01:47 -0300

    implementado criação de dados

--- fiap-chal-2/backend/db_factory_utils/db_create.py
+++ fiap-chal-2/backend/db_factory_utils/db_create.py
@@ -1,56 +1,57 @@
 import mysql.connector;
-import os;
 import datetime;
+import os;
 
-def createData(action,data):
-    appender="\",\""
-    if(kindofinfo=="newuser"):
-        rowstamp=now.strftime("%Y/%m/%d %H:%M:%S");
+def createData(kindofinfo, data, db_connection):
+    now = datetime.datetime.now()  # Obter a data/hora atual
+    rowstamp = now.strftime("%Y/%m/%d%H:%M:%S")
+    
+    # Inserção de novo usuário
+    if kindofinfo == "newuser":
+        column = 'uname, upwd, uendereco, rowstamp'
+        values = (data.get("uname"), data.get("upwd"), data.get("uendereco"), rowstamp)
+        sql = f"INSERT INTO tbuser ({column}) VALUES (%s, %s, %s, %s)"
+    
+    # Envio de email
+    elif kindofinfo == "sendemail":
+        column = 'usid, usdestinatario, eassunto, hasCal, ecompleto, egigante, rowstamp'
+        values = (
+            data.get("usid"), 
+            data.get("usdestinatario"), 
+            data.get("eassunto"), 
+            data.get("hasCal"), 
+            data.get("ecompleto"), 
+            data.get("egigante"), 
+            rowstamp
+        )
+        sql = f"INSERT INTO tbemail ({column}) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    
+    # Criação de evento de calendário
+    elif kindofinfo == "createcalendar":
+        column = 'usid, ueid, calinicio, calfinal, caldestinatario'
+        values = (
+            data.get("usid"), 
+            data.get("ueid"), 
+            data.get("calinicio"), 
+            data.get("calfinal"), 
+            data.get("caldestinatario")
+        )
+        sql = f"INSERT INTO tbcalendar ({column}) VALUES (%s, %s, %s, %s, %s)"
+    
+    else:
+        return "Tipo de informação inválida."
 
-        column='uname,upwd,uendereco,rowstamp'
-        values="\"" + data.get("uname") + appender + data.get("upwd") + appender + data.get(uendereco) + appender + rowstamp + "\""
+    # Inserir dados no banco de dados
+    try:
+        cursor = db_connection.cursor()
+        cursor.execute(sql, values)
+        db_connection.commit()  # Confirmar a transação
+        return f"{kindofinfo} inserido com sucesso"
+    except mysql.connector.Error as err:
+        return f"Error: {err}"
+    finally:
+        cursor.close()
 
-        response = db_connection.inserttx(column, values, tbuser)
-        return response
-
-    if(kindofinfo=="sendemail"):
-        rowstamp=now.strftime("%Y/%m/%d %H:%M:%S");
-
-        column='uname,upwd,uendereco,rowstamp'
-        values="\"" + data.get("uname") + appender + data.get("upwd") + appender + data.get(uendereco) + appender + rowstamp + "\""
-
-        response = db_connection.inserttx(column, values, tbuser)
-        return response
-
-    if(kindofinfo=="simplecal"):
-        rowstamp=now.strftime("%Y/%m/%d %H:%M:%S");
-
-        column='uname,upwd,uendereco,rowstamp'
-        values="\"" + data.get("uname") + appender + data.get("upwd") + appender + data.get(uendereco) + appender + rowstamp + "\""
-
-        response = db_connection.inserttx(column, values, tbuser)
-
-        return response
-
-    if(kindofinfo=="emailcal"):
-        rowstamp=now.strftime("%Y/%m/%d %H:%M:%S");
-
-        column='uname,upwd,uendereco,rowstamp'
-        values="\"" + data.get("uname") + appender + data.get("upwd") + appender + data.get(uendereco) + appender + rowstamp + "\""
-
-        response = db_connection.inserttx(column, values, tbuser)
-
-        return response
-
-    if(kindofinfo=="uconfig"):
-        rowstamp=now.strftime("%Y/%m/%d %H:%M:%S");
-
-        column='uname,upwd,uendereco,rowstamp'
-        values="\"" + data.get("uname") + appender + data.get("upwd") + appender + data.get(uendereco) + appender + rowstamp + "\""
-
-        response = db_connection.inserttx(column, values, tbuser)
-
-        return response
     
 
 #createUser:
