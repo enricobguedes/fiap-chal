a40666646a4fe5c1ee6c4c8bab30824fcfe4d5b0
Daniel Okuno <danielkenji13@yahoo.com.br>
2024-09-17 18:12:56 -0300

    implementado criação de dados2

--- fiap-chal-2/backend/db_factory_utils/db_create.py
+++ fiap-chal-2/backend/db_factory_utils/db_create.py
@@ -4,76 +4,41 @@ import os;
 
 def createData(kindofinfo, data, db_connection):
     now = datetime.datetime.now()  # Obter a data/hora atual
-    rowstamp = now.strftime("%Y/%m/%d%H:%M:%S")
-    
+    rowstamp = now.strftime("%Y/%m/%d %H:%M:%S")
+
     # Inserção de novo usuário
     if kindofinfo == "newuser":
         column = 'uname, upwd, uendereco, rowstamp'
-        values = (data.get("uname"), data.get("upwd"), data.get("uendereco"), rowstamp)
-        sql = f"INSERT INTO tbuser ({column}) VALUES (%s, %s, %s, %s)"
+        values = "\"" + data.get("uname") + "\",\"" + data.get("upwd") + "\",\"" + data.get("uendereco") + "\",\"" + rowstamp + "\""
+        table = "tbuser"
     
     # Envio de email
     elif kindofinfo == "sendemail":
         column = 'usid, usdestinatario, eassunto, hasCal, ecompleto, egigante, rowstamp'
-        values = (
-            data.get("usid"), 
-            data.get("usdestinatario"), 
-            data.get("eassunto"), 
-            data.get("hasCal"), 
-            data.get("ecompleto"), 
-            data.get("egigante"), 
-            rowstamp
-        )
-        sql = f"INSERT INTO tbemail ({column}) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        values = "\"" + data.get("usid") + "\",\"" + data.get("usdestinatario") + "\",\"" + data.get("eassunto") + "\",\"" + str(data.get("hasCal")) + "\",\"" + data.get("ecompleto") + "\",\"" + data.get("egigante") + "\",\"" + rowstamp + "\""
+        table = "tbemail"
     
     # Criação de evento de calendário
     elif kindofinfo == "createcalendar":
         column = 'usid, ueid, calinicio, calfinal, caldestinatario'
-        values = (
-            data.get("usid"), 
-            data.get("ueid"), 
-            data.get("calinicio"), 
-            data.get("calfinal"), 
-            data.get("caldestinatario")
-        )
-        sql = f"INSERT INTO tbcalendar ({column}) VALUES (%s, %s, %s, %s, %s)"
+        values = "\"" + data.get("usid") + "\",\"" + data.get("ueid") + "\",\"" + data.get("calinicio") + "\",\"" + data.get("calfinal") + "\",\"" + data.get("caldestinatario") + "\""
+        table = "tbcalendar"
     
-    else:
-        return "Tipo de informação inválida."
+    # Criação de tema do usuário
+    elif kindofinfo =="uconfig":
+        column = 'ucid', 'usid', 'uctema'
+        values ="\"" + data.get("ucid") + "\",\"" + data.get("usid") + "\",\"" + data.get("uctema") + "\""
+        table = "tbuserconfiog"
 
-    # Inserir dados no banco de dados
+     # Criação de evento de calendário
+    elif kindofinfo =="emailcal":
+        column = 'ucid', 'usid', 'uctema'
+        values ="\"" + data.get("ucid") + "\",\"" + data.get("usid") + "\",\"" + data.get("uctema") + "\""
+        table = "tbemailcal"
+
+    # Usar o método inserttx através do db_connection para inserir os dados
     try:
-        cursor = db_connection.cursor()
-        cursor.execute(sql, values)
-        db_connection.commit()  # Confirmar a transação
-        return f"{kindofinfo} inserido com sucesso"
-    except mysql.connector.Error as err:
+        response = db_connection.inserttx(column, values, table)
+        return response
+    except Exception as err:
         return f"Error: {err}"
-    finally:
-        cursor.close()
-
-    
-
-#createUser:
-## JSON esperado: 
-#    'uname'='USER_TESTE', 
-#    'upwd'= '01234567',
-#    'uendereco'='test@test.br',
-
-#sendEmail:
-## JSON esperado: 
-#    usid=user id,
-#    usdestinatario=email destinatario,  
-#    eassunto=assunto do email, 
-#    hasCal= boolean se tiver item de calendario, 
-#    ecompleto= corpo do email , 
-#    egigante= corpo do email se for maior que 2k char
-
-#createCalendar:
-## JSON esperado: 
-#    usid = user id 
-#    ueid = email id (retornado do sendEmail se tiver sido enviado em email) 
-#    calinicio = formato timestamp de SQL
-#    calfinal= formato timestamp de SQL
-#    caldestinatario = email de quem irá receber/quem convidou
-
